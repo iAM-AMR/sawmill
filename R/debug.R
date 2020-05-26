cce11fd4dce916237bf7ffb677cd5c907fc3fb85
Brennan Chapman <chapb@users.noreply.github.com>
2020-05-26 10:46:09 -0400

    Add function to calc. significance
    Add rough code to calculate the significance of the OR using a Fishers test

--- R/debug.R
+++ R/debug.R
@@ -1,20 +1,23 @@
 
+# This must be commented out to build the package.
 
-
-
-library("tidyverse")
-library("readxl")
-
-
-file_path  <- file.choose()
-raw_timber <- readxl::read_excel(file_path)
-
-timber1 <- debark(timber = raw_timber, cedar_version = 2)
-timber2 <- check_grain(timber = timber1)
-timber3 <- trim_scraps(timber = timber2, write_scrap = TRUE)
-timber4 <- build_table(timber = timber3,
-                       low_cell_correction = 0.5,
-                       low_cell_threshold = 0)
-timber5 <- add_CI(timber4)
-timber6 <- build_chairs(timber5)
-readr::write_csv(timber6, file.choose())
+# library("tidyverse")
+# library("readxl")
+#
+#
+# file_path  <- file.choose()
+# raw_timber <- readxl::read_excel(file_path)
+#
+# timber1 <- debark(timber = raw_timber, cedar_version = 2)
+# timber2 <- check_grain(timber = timber1)
+# timber3 <- trim_scraps(timber = timber2, write_scrap = TRUE)
+# timber4 <- build_table(timber = timber3,
+#                        low_cell_correction = 0.5,
+#                        low_cell_threshold = 0)
+# timber5 <- add_CI(timber4)
+#
+# timber5$oddsup <- as.numeric(timber5$oddsup)
+#
+# timber6 <- build_chairs(timber5, 10)
+# timber7 <- build_horse(timber6)
+# readr::write_csv(timber7, file.choose())

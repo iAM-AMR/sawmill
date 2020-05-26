0ef26aaf2cfcefd775ecaccf9a898b449bccb872
Brennan Chapman <chapb@users.noreply.github.com>
2020-05-26 10:46:42 -0400

    Add function to calc. significance (#2)
    Add rough code to calculate the significance of the OR using a Fishers test

---
+++ R/build_horse.R
@@ -0,0 +1,36 @@
+
+
+#' @title
+#'   Calculate the Significance of a Factor
+#'
+#' @description
+#'   \code{build_horse()} calculates the significance of the OR of a factor.
+#'
+#' @param timber
+#'   a tibble of timber, with a table built by \code{\link{build_table}}.
+#'
+#' @return
+#'   A tibble of timber with additional columns: \emph{pval}.
+#'
+#' @importFrom dplyr mutate
+#'
+#' @export
+
+
+
+build_horse <- function(timber) {
+
+  mutate(timber, pval = ifelse(grain %in% c("con_table_pos_neg", "con_table_pos_tot", "rate_table_pos_tot"),
+                               apply(timber, 1, fisher_p),
+                               oddsig))
+
+}
+
+
+
+fisher_p <- function(x) {
+
+  try(fisher.test(matrix(as.numeric(c(x[["A"]], x[["B"]], x[["C"]], x[["D"]])), nrow=2))$p.value,
+      silent = TRUE)
+
+}

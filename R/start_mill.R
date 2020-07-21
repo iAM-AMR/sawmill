94233fc2fdfda7613d328be4766484652f42d1e2
Charly Phillips <65024861+phillipsclynn@users.noreply.github.com>
2020-07-21 15:12:15 -0400

    Added error catching, changed messages to the user
    Error catching accomplished with sub_mill(), to identify potential errors in check_version(), get_col_types(), log_warnings(), handle_col_warnings(), and mill(), when called within this function. get_col_types(), log_warnings(), and handle_col_warnings() are used to check that input data meet the expected data types for each column, and, in the event that they do not, notify the user of which specific cells are causing such errors and give them the option to stop the pipeline and fix them. User messages were also changed to specify that the Enter key must be pressed to proceed past prompts that appear in the console.

--- R/start_mill.R
+++ R/start_mill.R
@@ -15,28 +15,41 @@
 
 start_mill <- function(){
 
-  readline(prompt="\nYou will now be asked to provide a CEDAR query. Press any key to continue.")
+  readline(prompt="\nYou will now be asked to provide a CEDAR query. Press any key, followed by the Enter key, to continue.")
 
   file_path  <- file.choose()
-  raw_timber <- readxl::read_excel(file_path)
 
-  user_vers  <- readline(prompt="\nWhat version of CEDAR is this export from? Enter 1 or 2 to continue:  ")
+  user_vers  <- readline(prompt="\nWhat version of CEDAR is this export from? Enter 1 or 2, followed by the Enter key, to continue:  ")
 
-  check_version(user_vers)
+  sub_mill(check_version(user_vers), "check_version")
 
-  furniture  <- mill(raw_timber, cedar_version = user_vers)
+  sub_mill(timber_col_types <- get_col_types(user_vers), "get_col_types")
+
+  sub_mill(log_warnings(raw_timber <- readxl::read_excel(file_path, col_types = timber_col_types)), "log_warnings")
+
+  tryCatch(raw_timber <- readxl::read_excel(file_path, col_types = timber_col_types),
+           warning = function(w) {
+             sub_mill(handle_col_warnings(), "handle_col_warnings")
+           })
+
+  sub_mill(furniture  <- mill(raw_timber, cedar_version = user_vers), "mill")
 
   readline(prompt = paste0("\n\nYou will now be asked to save the processed timber.\n",
                            "You MUST save the file with a .csv extension.\n",
-                           "Press any key to continue."))
+                           "Press any key, followed by the Enter key, to continue."))
 
   readr::write_csv(furniture, file.choose())
 
   readline(prompt = paste0("\nDone.\n",
-                           "Press any key to exit."))
+                           "Press any key, followed by the Enter key, to exit."))
   return(furniture)
 
 }
+
+
+
+
+
 
 
 

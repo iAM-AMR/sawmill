3bf84731adc7b23e4b6b503a8f59a8658902ebfa
Brennan Chapman <brennan@chapmanb.com>
2022-06-24 10:25:19 -0400

    Add update function

---
+++ R/update_sawmill.R
@@ -0,0 +1,32 @@
+
+
+# Update sawmill
+
+#' @title
+#'   Update sawmill
+#'
+#' @description
+#'
+#' @return
+#'
+#' @export
+
+
+
+update_sawmill <- function(){
+
+  detach("package:sawmill")
+
+  usePackage <- function(p) {
+    if (!is.element(p, installed.packages()[,1])) install.packages(p, dep = TRUE)
+    library(p, character.only = TRUE)
+  }
+
+  usePackage("remotes")
+
+  remotes::install_github("iAM-AMR/sawmill@*release")
+
+
+}
+
+
